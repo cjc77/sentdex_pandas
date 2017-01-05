64e01ae2c089f7d71a5f1c7001eae1f03155bc78
cjc77 <carsonjamescook@gmail.com>
2017-01-04 16:21:10 -0800

    Updated demo1.py

--- demo1.py
+++ demo1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import datetime
-import pandas_datareader.data as web
+# import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
